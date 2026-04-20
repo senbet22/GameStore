49a0e78882ab6deaf877dcdacd600ec4b64a8e3f
senbet22 <senbet22@users.noreply.github.com>
2026-04-20 11:01:42 +0200

    added Game and Genre domain models

---
+++ Gamestore.API/Models/Game.cs
@@ -0,0 +1,19 @@
+namespace Gamestore.API.Models;
+
+public class Game
+{
+  public int Id {get; set;}
+
+  public required string Name {get; set;}
+
+  public Genre? Genre {get; set;}
+
+  public int GenreId {get; set;}
+
+  public decimal Price {get; set;}
+
+  public DateOnly ReleaseDate {get; set;}
+  
+  public required string Studio {get; set;}
+
+}
---
+++ Gamestore.API/Models/Genre.cs
@@ -0,0 +1,10 @@
+namespace Gamestore.API.Models;
+
+public class Genre
+{
+    public int Id {get; set;}
+
+    public required string Name {get; set;}
+
+
+}
